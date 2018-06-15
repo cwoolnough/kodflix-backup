4f85a2c9f59a1f3c6f3c1ce9c8475807ed39e7ee
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-06-15 18:50:41 +0100

    Added name to h1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Kodflix</h1>
+          <h1 className="App-title">Welcome Chris to Kodflix</h1>
         </header>
         <p className="App-intro">
           How to get started, edit <code>src/App.js</code> and save to reload.

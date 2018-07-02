eb03fa9b59556551077f420ffd8f791720348914
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-07-02 11:46:14 +0100

    added favourite movie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BadBoys from './imgs/badboys.jpg';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           How to get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={BadBoys} alt="Bad Boys" className="BadBoys"/>;
       </div>
     );
   }

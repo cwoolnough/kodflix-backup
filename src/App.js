7f02192bea2c9bc40d414cad552a10e2228a35c2
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-07-03 10:26:32 +0100

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -19,18 +19,36 @@ class App extends Component {
         
 
         <div className = "flex-container">
-          <div className= "item"><img src ={breakingBad} alt="Breaking Bad"/></div>
-          <div className= "item"><img src ={blackMirror} alt="Black Mirror"/></div>
-          <div className= "item"><img src ={deathNote} alt="Death Note"/></div>
+          <div className= "item">
+            <img src ={breakingBad} alt="Breaking Bad"/>
+            <div className = "title"><h1>Breaking Bad</h1></div>
+          </div>
+          <div className= "item">
+            <img src ={blackMirror} alt="Black Mirror"/>
+            <div className = "title"><h1>Black Mirror</h1></div>
+          </div>
+          <div className= "item">
+            <img src ={deathNote} alt="Death Note"/>
+            <div className = "title"><h1>Death Note</h1></div> 
+          </div>
         </div>
 
         <div className = "flex-container">  
-          <div className= "item"><img src = {GOT} alt="Game of Throne" /></div>
-          <div className= "item"><img src = {theWire} alt="The Walking Dead" /></div>
-          <div className= "item"><img src = {walkingDead} alt="Wire" /></div>
+          <div className= "item">
+            <img src = {GOT} alt="Game of Throne" />
+            <div className = "title"><h1>Game of Thrones</h1></div>
+          </div> 
+          <div className= "item">
+            <img src = {theWire} alt="The Wire" />
+            <div className = "title"><h1>The Wire</h1></div>
+          </div>
+          <div className= "item">
+            <img src = {walkingDead} alt="Walking Dead" />
+            <div className = "title"><h1>Walking Dead</h1></div>
+          </div>
         </div>
-
       </div>
+    
     );
   }
 }
